fd9c81b0173e40dbce9acfa246c1df88afba42f6
brzemek <01187278@pw.edu.pl>
2024-12-29 18:48:25 +0100

    Added methods to modify the accounts details

--- src/main/java/com/example/listing/AccountListing.java
+++ src/main/java/com/example/listing/AccountListing.java
@@ -54,6 +54,87 @@ public class AccountListing {
         return success;
     }
 
+    public boolean changeLogin(int accountId, String newLogin) {
+        // Call the repository method to update the login
+        boolean success = AccountRepository.changeLogin(accountId, newLogin);
+        if (success) {
+            // Reload the updated list of accounts if the login change is successful
+            loadAllAccounts();
+        }
+        return success;
+    }
+
+    public boolean changePassword(int accountId, String newPassword) {
+        // Call the repository method to update the password
+        boolean success = AccountRepository.changePassword(accountId, newPassword);
+        if (success) {
+            // Reload the updated list of accounts if the password change is successful
+            loadAllAccounts();
+        }
+        return success;
+    }
+
+    public boolean changeEmail(int accountId, String newEmail) {
+        // Call the repository method to update the email
+        boolean success = AccountRepository.changeEmail(accountId, newEmail);
+        if (success) {
+            // Reload the updated list of accounts if the email change is successful
+            loadAllAccounts();
+        }
+        return success;
+    }
+    
+    public boolean changePhoneNumber(int accountId, String newPhoneNumber) {
+        // Call the repository method to update the phone number
+        boolean success = AccountRepository.changePhone(accountId, newPhoneNumber);
+        if (success) {
+            // Reload the updated list of accounts if the phone number change is successful
+            loadAllAccounts();
+        }
+        return success;
+    }
+        
+    public boolean addPoints(int accountId, int points) {
+        // Call the repository method to add points
+        boolean success = AccountRepository.addLoyaltyPoints(accountId, points);
+        if (success) {
+            // Reload the updated list of accounts if adding points was successful
+            loadAllAccounts();
+        }
+        return success;
+    }
+    
+    public boolean takePoints(int accountId, int points) {
+        // Call the repository method to add points
+        boolean success = AccountRepository.takeLoyaltyPoints(accountId, points);
+        if (success) {
+            // Reload the updated list of accounts if adding points was successful
+            loadAllAccounts();
+        }
+        return success;
+    }
+
+    public boolean addBalance(int accountId, double balance) {
+        // Call the repository method to add points
+        boolean success = AccountRepository.addBalance(accountId, balance);
+        if (success) {
+            // Reload the updated list of accounts if adding points was successful
+            loadAllAccounts();
+        }
+        return success;
+    }
+    
+    public boolean takeBalance(int accountId, double balance) {
+        // Call the repository method to add points
+        boolean success = AccountRepository.takeBalance(accountId, balance);
+        if (success) {
+            // Reload the updated list of accounts if adding points was successful
+            loadAllAccounts();
+        }
+        return success;
+    }
+
+
     // Get all accounts
     public List<Account> getAllAccounts() {
         return new ArrayList<>(accounts); // Return a copy of the accounts list
