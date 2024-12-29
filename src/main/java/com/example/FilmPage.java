ae42d49e444089e4810e67a0a217b4d476f4fe7e
Mikołaj <mikolajwiniarski2.1@gmail.com>
2024-12-29 19:26:11 +0100

    made showing in filmPage as List

--- src/main/java/com/example/FilmPage.java
+++ src/main/java/com/example/FilmPage.java
@@ -8,6 +8,8 @@ import com.example.database.db_classes.Showing;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -23,17 +25,25 @@ public class FilmPage implements Page {
             actors = actors.concat(actor.toString() + " ");
         }
         System.out.println(actors);
-        List<Showing> showings = filmInfo.getShowings();
-        String showTimes = "";
-        for (Showing showing : showings) {
-            showTimes = showTimes.concat(showing.getShowTime() + "\n");
-        }
+
+
+        ListView<Showing> showingsListView = new ListView<>();
+        showingsListView.getStyleClass().add("lists");
+        showingsListView.getItems().addAll(filmInfo.getShowings());
+        showingsListView.setCellFactory(listView -> new ShowingListCell(controller));
+
+        double rowHeight = 24;
+        showingsListView.setPrefHeight(filmInfo.getShowings().size() * rowHeight);
+
+        VBox showingsBox = new VBox(showingsListView);
+
+
         HBox titleRow = this.getRow("Title", filmInfo.getTitle());
         HBox ratingRow = this.getRow("Rating", String.valueOf(filmInfo.getRating()));
         HBox shortDescRow = this.getRow("Short Description", filmInfo.getShortDescription());
         HBox longDescRow = this.getRow("Description", filmInfo.getLongDescription());
+        HBox showingsRow = this.getRow("Showings:", "");
         HBox actorsRow = this.getRow("Actors", actors);
-        HBox showingsRow = this.getRow("Showings", showTimes);
         Button backButton = new Button("Back");
         backButton.getStyleClass().add("back-btn");
         backButton.setId("repertoireBackBtn");
@@ -41,7 +51,7 @@ public class FilmPage implements Page {
 
         filmPage = new VBox();
         filmPage.getStyleClass().add("film-page");
-        filmPage.getChildren().addAll(titleRow, ratingRow, shortDescRow, longDescRow, actorsRow, showingsRow, backButton);
+        filmPage.getChildren().addAll(titleRow, ratingRow, shortDescRow, longDescRow, actorsRow, showingsRow, showingsBox, backButton);
     }
 
     private HBox getRow(String attribute, String value) {
@@ -53,6 +63,35 @@ public class FilmPage implements Page {
         row.getChildren().addAll(attrLabel, valueLabel);
         return row;
     }
+
+    private static class ShowingListCell extends ListCell<Showing> {
+    private final Controller controller;
+
+    public ShowingListCell(Controller controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    protected void updateItem(Showing showing, boolean empty) {
+        super.updateItem(showing, empty);
+
+        if (empty || showing == null) {
+            setText(null);
+            setGraphic(null);
+        } else {
+            VBox content = new VBox();
+
+            Label showTimeLabel = new Label("Showtime: " + showing.getShowTime());
+
+            this.setOnMouseClicked(e -> {
+                System.out.println("Clicked showing: " + showing.getShowTime());
+            });
+
+            content.getChildren().addAll(showTimeLabel);
+            setGraphic(content);
+        }
+    }
+}
 
     public VBox getPage() {
         return filmPage;
